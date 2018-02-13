1ed10967592b2a2b13e7a4ce7f04cfd90a131cf8
i.chelyshkin <i.chelyshkin@corp.mail.ru>
2018-02-13 20:17:37 +0300

    update script

--- environments/venv-additional-libs.sh
+++ environments/venv-additional-libs.sh
@@ -2,21 +2,52 @@
 
 #Script install additional python libs using pip
 
-#Script takes a value: env_name, lib_name
-#As default value use pattern: venv_[a-z]*
+#Script takes a value: -n app_name -v env_name, -p pkg_name
 
 #Example: 
-#venv-additional-lib.sh venv_projectname django-resized
+#venv-additional-lib.sh -n project -v .venv_projectname -p django-resized
 
-VENV_FOLDER=$1
-PYTHON_LIB=$2
+#----------------------------------------------------------------------------
+#Check options
 
-if [[ $VENV_FOLDER = "" ]]; then
-    exit 1
+while [[ $# -gt 1 ]]
+do
+	key="$1"
+	case $key in
+		-n|--name)
+		APP_NAME=$2
+		shift ;;
+		-v|--venv)
+		VENV_FOLDER=$2
+		shift ;;
+		-p|--pkg)
+		PYTHON_PKG=$2
+		shift ;;
+	esac
+	shift
+done
+
+if [[ $APP_NAME = "" ]]; then
+	echo "ERR: forgot APP_NAME attr."
+	exit 1
 fi
 
-venv_path="/home/hotdog/$VENV_FOLDER"
+if [[ $VENV_FOLDER = "" ]]; then
+	echo "ERR: forgot VENV_FOLDER attr."
+	exit 1
+fi
+
+if [[ $PYTHON_PKG = "" ]]; then
+	echo "ERR: forgot PYTHON_PKG attr."
+	exit 1
+fi
+
+
+#----------------------------------------------------------------------------
+#Install pkg
+
+venv_path="/home/hotdog/$APP_NAME/$VENV_FOLDER"
 
 source $venv_path/bin/activate
 
-pip install $PYTHON_LIB
+pip install $PYTHON_PKG
