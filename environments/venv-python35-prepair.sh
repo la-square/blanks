2e2b63e9e46b4c0e8d48962b8b584fc1ca5610b5
i.chelyshkin <i.chelyshkin@corp.mail.ru>
2018-02-13 19:59:22 +0300

    add option parser

--- environments/venv-python35-prepair.sh
+++ environments/venv-python35-prepair.sh
@@ -3,23 +3,50 @@
 #Creating python virtual environmant
 #Install require libs
 
-#Script takes a value: env_name
-#As default value use pattern: venv_[a-z]*
+#Script takes a value: -n project_name, -v env_name
+#As default value use pattern: .venv_[a-z]*
 
 #Example: 
-#venv-python35-prepair.sh venv_projectname
+#venv-python35-prepair.sh -n project -v .venv_projectname
 
-VENV_FOLDER=$1
+#----------------------------------------------------------------------------
+#Check options
 
-if [[ $VENV_FOLDER = "" ]]; then
-    exit 1
+while [[ $# -gt 1 ]]
+do
+	key="$1"
+	case $key in
+		-n|--name)
+		APP_NAME=$2
+		shift ;;
+		-v|--venv)
+		VENV_FOLDER=$2
+		shift ;;
+	esac
+	shift
+done
+
+if [[ $APP_NAME = "" ]]; then
+	echo "ERR: forgot APP_NAME attr."
 fi
 
-venv_path="/home/hotdog/$VENV_FOLDER"
+if [[ $VENV_FOLDER = "" ]]; then
+	echo "ERR: forgot VENV_FOLDER attr."
+fi
+
+
+#----------------------------------------------------------------------------
+#Create virtual python
+
+venv_path="/home/hotdog/$APP_NAME/$VENV_FOLDER"
 
 rm -rf $venv_path && mkdir $venv_path
 python3.5 -m venv $venv_path
 source $venv_path/bin/activate
+
+
+#----------------------------------------------------------------------------
+#Install required pkgs
 
 pip install django==1.9
 pip install psycopg2==2.7.1
