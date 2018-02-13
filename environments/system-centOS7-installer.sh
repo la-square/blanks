ae86a2f1747447ebf49f6872fca8490b2b9e83b6
i.chelyshkin <i.chelyshkin@corp.mail.ru>
2018-02-13 15:56:41 +0300

    init system-centOS7-installer

---
+++ environments/system-centOS7-installer.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+
+#Installing a set of utilities for the django web-server
+# - system user
+# - nginx
+# - postgres
+# - systemd
+# - python35
+# - custom vim
+
+
+#----------------------------------------------------------------------------
+#Package install
+
+yes Y | yum install epel-release
+yes Y | yum update
+
+yes Y | yum install gcc
+yes Y | yum install systemd
+yes Y | yum install memcached
+
+yes Y | yum install postgresql-server
+yes Y | yum install postgresql-devel
+yes Y | yum install postgresql-contrib
+
+yes Y | yum -y install https://centos7.iuscommunity.org/ius-release.rpm
+yes Y | yum -y install python35u
+yes Y | yum -y install python35u-pip
+
+yum -y install nginx
+
+
+#----------------------------------------------------------------------------
+#Prapair work environment
+
+#-> vim configs
+wget -O ~/.vimrc http://dumpz.org/25712/nixtext/
+update-alternatives --set editor /usr/bin/vim.basic
+
+#-> prepair user
+useradd -m hotdog -s /bin/bash
+cp ~/.vimrc /home/hotdog
+
+chown -R hotdog:hotdog /home/hotdog
+
+
+#----------------------------------------------------------------------------
+#Prapair system services
+
+#-> postgres
+sed -i "s/ident/md5/g" /var/lib/pgsql/data/pg_hba.conf
+postgresql-setup initdb
+
+systemctl enable postgresql
+systemctl start postgresql.service
+
+#-> nginx
+systemctl enable nginx
+service start nginx.service
