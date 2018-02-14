044c5627809da26fcf8343d5f85aab877c373317
i.chelyshkin <i.chelyshkin@corp.mail.ru>
2018-02-14 20:20:38 +0300

    init basic nginx config state

---
+++ environments/nginx-basic-config.sh
@@ -0,0 +1,161 @@
+#!/usr/bin/env bash
+
+#Script create basic settings of nginx service
+
+#Example:
+#nginx-basic-config.sh
+
+#Require:
+#installed nginx service
+
+#----------------------------------------------------------------------------
+#Init mime_types.conf
+
+rm -rf /etc/nginx/conf.d/mime_types.conf
+touch /etc/nginx/conf.d/mime_types.conf
+
+cat >> /etc/nginx/conf.d/mime_types.conf << EOF
+types {
+    text/html                             html htm shtml;
+    text/css                              css;
+    text/xml                              xml;
+    image/gif                             gif;
+    image/jpeg                            jpeg jpg;
+    application/javascript                js;
+    application/atom+xml                  atom;
+    application/rss+xml                   rss;
+
+    text/mathml                           mml;
+    text/plain                            txt;
+    text/vnd.sun.j2me.app-descriptor      jad;
+    text/vnd.wap.wml                      wml;
+    text/x-component                      htc;
+
+    image/png                             png;
+    image/tiff                            tif tiff;
+    image/vnd.wap.wbmp                    wbmp;
+    image/x-icon                          ico;
+    image/x-jng                           jng;
+    image/x-ms-bmp                        bmp;
+    image/svg+xml                         svg svgz;
+    image/webp                            webp;
+
+    application/font-woff                 woff;
+    application/java-archive              jar war ear;
+    application/json                      json;
+    application/mac-binhex40              hqx;
+    application/msword                    doc;
+    application/pdf                       pdf;
+    application/postscript                ps eps ai;
+    application/rtf                       rtf;
+    application/vnd.apple.mpegurl         m3u8;
+    application/vnd.ms-excel              xls;
+    application/vnd.ms-fontobject         eot;
+    application/vnd.ms-powerpoint         ppt;
+    application/vnd.wap.wmlc              wmlc;
+    application/vnd.google-earth.kml+xml  kml;
+    application/vnd.google-earth.kmz      kmz;
+    application/x-7z-compressed           7z;
+    application/x-cocoa                   cco;
+    application/x-java-archive-diff       jardiff;
+    application/x-java-jnlp-file          jnlp;
+    application/x-makeself                run;
+    application/x-perl                    pl pm;
+    application/x-pilot                   prc pdb;
+    application/x-rar-compressed          rar;
+    application/x-redhat-package-manager  rpm;
+    application/x-sea                     sea;
+    application/x-shockwave-flash         swf;
+    application/x-stuffit                 sit;
+    application/x-tcl                     tcl tk;
+    application/x-x509-ca-cert            der pem crt;
+    application/x-xpinstall               xpi;
+    application/xhtml+xml                 xhtml;
+    application/xspf+xml                  xspf;
+    application/zip                       zip;
+
+    application/octet-stream              bin exe dll;
+    application/octet-stream              deb;
+    application/octet-stream              dmg;
+    application/octet-stream              iso img;
+    application/octet-stream              msi msp msm;
+
+    application/vnd.openxmlformats-officedocument.wordprocessingml.document    docx;
+    application/vnd.openxmlformats-officedocument.spreadsheetml.sheet          xlsx;
+    application/vnd.openxmlformats-officedocument.presentationml.presentation  pptx;
+
+    audio/midi                            mid midi kar;
+    audio/mpeg                            mp3;
+    audio/ogg                             ogg;
+    audio/x-m4a                           m4a;
+    audio/x-realaudio                     ra;
+
+    video/3gpp                            3gpp 3gp;
+    video/mp2t                            ts;
+    video/mp4                             mp4;
+    video/mpeg                            mpeg mpg;
+    video/quicktime                       mov;
+    video/webm                            webm;
+    video/x-flv                           flv;
+    video/x-m4v                           m4v;
+    video/x-mng                           mng;
+    video/x-ms-asf                        asx asf;
+    video/x-ms-wmv                        wmv;
+    video/x-msvideo                       avi;
+}
+
+EOF
+
+
+#----------------------------------------------------------------------------
+#Init nginx.conf
+
+rm -rf /etc/nginx/nginx.conf
+touch /etc/nginx/nginx.conf
+
+mkdir /etc/nginx/sites-available
+mkdir /etc/nginx/sites-enabled
+
+chmod 0744 /etc/nginx/nginx.conf
+cat >> /etc/nginx/nginx.conf << EOF
+user nginx;
+
+worker_processes auto;
+worker_rlimit_nofile 65535;
+
+pid /run/nginx.pid;
+error_log /var/log/nginx/error.log;
+
+events {
+	worker_connections 40960;
+	accept_mutex off;
+}
+
+http {
+	sendfile on;
+	server_tokens off;
+	map_hash_max_size 4096;
+	types_hash_max_size 2048;
+	server_names_hash_bucket_size 512;
+
+	include conf.d/mime_types.conf;
+	default_type application/octet-stream;
+
+	access_log /var/log/nginx/access.log combined buffer=4m flush=1s;
+	error_log /var/log/nginx/error.log;
+
+	client_body_buffer_size 1M;
+	client_header_buffer_size 1M;
+	large_client_header_buffers 128 8k;
+
+	open_file_cache max=4096;
+	include sites-enabled/*;
+}
+
+EOF
+
+#----------------------------------------------------------------------------
+#Restart daemon
+
+systemctl daemon-reload
+systemctl restart nginx.service
