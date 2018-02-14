98e3c78a49ef5908516c8cf5656bbdd97285ca46
i.chelyshkin <i.chelyshkin@corp.mail.ru>
2018-02-14 20:37:41 +0300

    refactor nginx basic confs

--- environments/nginx-basic-config.sh
+++ environments/nginx-basic-config.sh
@@ -9,112 +9,35 @@
 #installed nginx service
 
 #----------------------------------------------------------------------------
-#Init mime_types.conf
+#Prepair nginx
 
-rm -rf /etc/nginx/conf.d/mime_types.conf
-touch /etc/nginx/conf.d/mime_types.conf
+mkdir /etc/nginx/sites-available
+mkdir /etc/nginx/sites-enabled
 
-cat >> /etc/nginx/conf.d/mime_types.conf << EOF
-types {
-    text/html                             html htm shtml;
-    text/css                              css;
-    text/xml                              xml;
-    image/gif                             gif;
-    image/jpeg                            jpeg jpg;
-    application/javascript                js;
-    application/atom+xml                  atom;
-    application/rss+xml                   rss;
+mkdir /etc/nginx/conf.d
+mv /etc/nginx/mime.types /etc/nginx/conf.d
 
-    text/mathml                           mml;
-    text/plain                            txt;
-    text/vnd.sun.j2me.app-descriptor      jad;
-    text/vnd.wap.wml                      wml;
-    text/x-component                      htc;
+rm -rf /etc/nginx/fastcgi.conf
+rm -rf /etc/nginx/fastcgi.conf.default
 
-    image/png                             png;
-    image/tiff                            tif tiff;
-    image/vnd.wap.wbmp                    wbmp;
-    image/x-icon                          ico;
-    image/x-jng                           jng;
-    image/x-ms-bmp                        bmp;
-    image/svg+xml                         svg svgz;
-    image/webp                            webp;
+rm -rf /etc/nginx/fastcgi_params
+rm -rf /etc/nginx/fastcgi_params.default
 
-    application/font-woff                 woff;
-    application/java-archive              jar war ear;
-    application/json                      json;
-    application/mac-binhex40              hqx;
-    application/msword                    doc;
-    application/pdf                       pdf;
-    application/postscript                ps eps ai;
-    application/rtf                       rtf;
-    application/vnd.apple.mpegurl         m3u8;
-    application/vnd.ms-excel              xls;
-    application/vnd.ms-fontobject         eot;
-    application/vnd.ms-powerpoint         ppt;
-    application/vnd.wap.wmlc              wmlc;
-    application/vnd.google-earth.kml+xml  kml;
-    application/vnd.google-earth.kmz      kmz;
-    application/x-7z-compressed           7z;
-    application/x-cocoa                   cco;
-    application/x-java-archive-diff       jardiff;
-    application/x-java-jnlp-file          jnlp;
-    application/x-makeself                run;
-    application/x-perl                    pl pm;
-    application/x-pilot                   prc pdb;
-    application/x-rar-compressed          rar;
-    application/x-redhat-package-manager  rpm;
-    application/x-sea                     sea;
-    application/x-shockwave-flash         swf;
-    application/x-stuffit                 sit;
-    application/x-tcl                     tcl tk;
-    application/x-x509-ca-cert            der pem crt;
-    application/x-xpinstall               xpi;
-    application/xhtml+xml                 xhtml;
-    application/xspf+xml                  xspf;
-    application/zip                       zip;
+rm -rf /etc/nginx/nginx.conf
+rm -rf /etc/nginx/nginx.conf.default
 
-    application/octet-stream              bin exe dll;
-    application/octet-stream              deb;
-    application/octet-stream              dmg;
-    application/octet-stream              iso img;
-    application/octet-stream              msi msp msm;
+rm -rf /etc/nginx/uwsgi_params
+rm -rf /etc/nginx/uwsgi_params.default
 
-    application/vnd.openxmlformats-officedocument.wordprocessingml.document    docx;
-    application/vnd.openxmlformats-officedocument.spreadsheetml.sheet          xlsx;
-    application/vnd.openxmlformats-officedocument.presentationml.presentation  pptx;
+rm -rf /etc/nginx/scgi_params
+rm -rf /etc/nginx/scgi_params.default
 
-    audio/midi                            mid midi kar;
-    audio/mpeg                            mp3;
-    audio/ogg                             ogg;
-    audio/x-m4a                           m4a;
-    audio/x-realaudio                     ra;
-
-    video/3gpp                            3gpp 3gp;
-    video/mp2t                            ts;
-    video/mp4                             mp4;
-    video/mpeg                            mpeg mpg;
-    video/quicktime                       mov;
-    video/webm                            webm;
-    video/x-flv                           flv;
-    video/x-m4v                           m4v;
-    video/x-mng                           mng;
-    video/x-ms-asf                        asx asf;
-    video/x-ms-wmv                        wmv;
-    video/x-msvideo                       avi;
-}
-
-EOF
-
+rm -rf /etc/nginx/default.d
 
 #----------------------------------------------------------------------------
 #Init nginx.conf
 
-rm -rf /etc/nginx/nginx.conf
 touch /etc/nginx/nginx.conf
-
-mkdir /etc/nginx/sites-available
-mkdir /etc/nginx/sites-enabled
 
 chmod 0744 /etc/nginx/nginx.conf
 cat >> /etc/nginx/nginx.conf << EOF
