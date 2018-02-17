bc295060fcf94fbfcdf6481cefb5ae056517c601
i.chelyshkin <i.chelyshkin@corp.mail.ru>
2018-02-17 22:48:49 +0300

    add uwsgi init and refactor

--- deploy-template/basic-deploy.sh
+++ deploy-template/basic-deploy.sh
@@ -2,8 +2,9 @@
 
 #Script deploy basic web-application, implements blanks deploy and blanks-applications
 
-#----->
+#--------------------------------------------->
 #SETTINGS
+#--------------------------------------------->
 
 project_name="enter_project_name"
 host="enter_hostname"
@@ -12,18 +13,26 @@ db_passwd="enter_dbpass"
 #add blank components for deploing
 components=(core)
 
-#----->
+
+#--------------------------------------------->
 #DEPLOY
+#--------------------------------------------->
 django_path="/home/hotdog/$project_name/djapp"
 states_path="/root/blanks-deploy"
 components_path="/root/blanks-applications"
+
 git clone git@github.com:la-square/blanks-deploy.git
 
+
+#----->
 #deploy system pkgs
 $states_path/system/centOS7-installer.sh 	-n $project_name
+
 $states_path/python35/django-prepair.sh     -n $project_name
 $states_path/postgres/psql-init-db.sh       -n db_$project_name 	-u hotdog 	-p $db_passwd
 
+
+#----->
 #init project
 git clone git@github.com:la-square/blanks-applications.git
 
@@ -32,10 +41,20 @@ do
 	cp -r $components_path/$app/* $django_path/
 done
 
-#init nginx and uwsgi
+
+#----->
+#init uwsgi
+$states_path/uwsgi/uwsgi-basic-config.sh
+$states_path/uwsgi/uwsgi-basic-config.sh 	-n $project_name
+
+
+#----->
+#init nginx
 $states_path/nginx/nginx-basic-config.sh
 $states_path/nginx/nginx-http-server.sh     -n $project_name     -h $host
 
+
+#----->
 #remove artifacts
 rm -rf $states_path
 rm -rf $components_path
