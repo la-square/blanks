95e0018cc457c8f329b2a56808dee335ffad3ee3
i.chelyshkin <i.chelyshkin@corp.mail.ru>
2018-02-25 14:11:29 +0300

    update stdout

--- uwsgi/uwsgi-basic-config.sh
+++ uwsgi/uwsgi-basic-config.sh
@@ -10,6 +10,11 @@
 #Require:
 #installed python-uwsgi -n project_name
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+NC='\033[0m'
+
 #----------------------------------------------------------------------------
 #Check options
 
@@ -25,22 +30,25 @@ do
 done
 
 if [[ $APP_NAME = "" ]]; then
-    echo "ERR: forgot APP_NAME attr."
-    exit 1
+	printf "${RED}ERR: forgot APP_NAME attr${NC}\n"
+	printf "${RED}system-centOS7-installer.sh was skipped.${NC}\n"
+	exit 1
 fi
 
 
 #----------------------------------------------------------------------------
-#Prepair uwsgi comfig
+#Prepair uwsgi config
 
-mkdir -p /etc/uwsgi/vassals
+printf "${CYAN}Init uwsgi main config...${NC}\n"
+
+mkdir -p /etc/uwsgi/vassals_$APP_NAME      2>&1 > /dev/null
 rm -rf /etc/uwsgi/main_uwsgi.ini
 
 touch /etc/uwsgi/main_uwsgi.ini
-cat >> /etc/uwsgi/main_uwsgi.ini << EOF
+cat >> /etc/uwsgi/main_uwsgi_$APP_NAME.ini << EOF
 [uwsgi]
-main_uwsgi = /etc/uwsgi/vassals
-pidfile=/run/.${APP_NAME}_uwsgi.pid
+main_uwsgi = /etc/uwsgi/vassals_$APP_NAME
+pidfile=/run/.uwsgi_$APP_NAME.pid
 uid = hotdog
 gid = hotdog
 master = true
@@ -53,6 +61,8 @@ EOF
 #----------------------------------------------------------------------------
 #Prepair uwsgi daemon
 
+printf "${CYAN}Init uwsgi service...${NC}\n"
+
 rm -rf /etc/systemd/system/uwsgi_$APP_NAME.service
 touch /etc/systemd/system/uwsgi_$APP_NAME.service
 cat >> /etc/systemd/system/uwsgi_$APP_NAME.service<< EOF
@@ -61,8 +71,8 @@ Description=uWSGI
 After=syslog.target
 
 [Service]
-ExecStart=/home/hotdog/$APP_NAME/.venv_$APP_NAME/bin/uwsgi --ini /etc/uwsgi/main_uwsgi.ini \
---emperor /etc/uwsgi/vassals
+ExecStart=/home/hotdog/$APP_NAME/.venv_$APP_NAME/bin/uwsgi --ini /etc/uwsgi/main_uwsgi_$APP_NAME.ini \
+--emperor /etc/uwsgi/vassals_$APP_NAME
 
 RuntimeDirectory=uwsgi
 Restart=always
@@ -81,4 +91,11 @@ EOF
 chown -R hotdog:hotdog /home/hotdog
 
 systemctl daemon-reload
-systemctl restart uwsgi_$APP_NAME.service
+systemctl restart uwsgi_$APP_NAME.service 2>/dev/shm/c1stderr
+if [ "$?" -ne "0" ]; then
+	err=$(cat /dev/shm/c1stderr)
+	printf "${RED}$err${NC}\n"
+else 
+	printf "uwsgi.service       ${GREEN}active${NC}\n"
+fi
+
