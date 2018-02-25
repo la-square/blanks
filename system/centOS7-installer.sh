81cfa34631adbf9afbecf783c0e70376f54fd90d
i.chelyshkin <i.chelyshkin@corp.mail.ru>
2018-02-25 13:24:45 +0300

    update script outputs

--- system/centOS7-installer.sh
+++ system/centOS7-installer.sh
@@ -109,12 +109,7 @@ else
 	printf "postgresql...   ${GREEN}ok${NC}\n"
 fi
 
-yes Y | yum -y install https://centos7.iuscommunity.org/ius-release.rpm 2>&1 > /dev/null 2>/dev/shm/c1stderr
-if [ "$?" -ne "0" ]; then
-	err=$(cat /dev/shm/c1stderr)
-	printf "${RED}$err${NC}\n"
-fi
-
+yes Y | yum -y install https://centos7.iuscommunity.org/ius-release.rpm 2>&1 > /dev/null 2>/dev/null
 yes Y | yum -y install python35u 				2>&1 > /dev/null 2>/dev/shm/c1stderr
 if [ "$?" -ne "0" ]; then
 	err=$(cat /dev/shm/c1stderr)
@@ -148,19 +143,19 @@ fi
 printf "${CYAN}Init work environment...${NC}\n"
 
 #-> vim configs
-wget -O ~/.vimrc http://dumpz.org/25712/nixtext/ 	2>&1 > /dev/null
-update-alternatives --set editor /usr/bin/vim.basic 2>&1 > /dev/null
+wget -O ~/.vimrc http://dumpz.org/25712/nixtext/ 	--quiet
+update-alternatives --set editor /usr/bin/vim.basic 
 printf "vim configs...  ${GREEN}ok${NC}\n"
 
 #-> prepair user
-useradd -m hotdog -s /bin/bash						2>&1 > /dev/null
+useradd -m hotdog -s /bin/bash						2>&1 > /dev/null 2>/dev/null
 
 mkdir -p /home/hotdog/$APP_NAME/djapp 				2>&1 > /dev/null
 mkdir -p /home/hotdog/$APP_NAME/media				2>&1 > /dev/null
 mkdir -p /home/hotdog/$APP_NAME/logs				2>&1 > /dev/null
 
 chown -R hotdog:hotdog /home/hotdog 				2>&1 > /dev/null
-printf "prepair user...  ${GREEN}ok${NC}\n"
+printf "prepair user... ${GREEN}ok${NC}\n"
 
 printf "done.\n"
 
@@ -168,17 +163,26 @@ printf "done.\n"
 #----------------------------------------------------------------------------
 #Prapair system services
 
-printf "${CYAN}Init postgres...${NC}\n"
+printf "${CYAN}Init services...${NC}\n"
 
 #-> postgres
 sed -i "s/ident/md5/g" /var/lib/pgsql/data/pg_hba.conf
 postgresql-setup initdb 2>&1 > /dev/null
 
-printf "done.\n"
 
-systemctl start postgresql.service
-printf "${GREEN}Start postgresql.service${NC}\n"
+systemctl start postgresql.service 2>/dev/shm/c1stderr
+if [ "$?" -ne "0" ]; then
+	err=$(cat /dev/shm/c1stderr)
+	printf "${RED}$err${NC}\n"
+else 
+	printf "${GREEN}Start postgresql.service${NC}\n"
+fi
 
 #-> nginx
-systemctl start nginx.service
-printf "${GREEN}Start nginx.service${NC}\n"
+systemctl start nginx.service 2>/dev/shm/c1stderr
+if [ "$?" -ne "0" ]; then
+	err=$(cat /dev/shm/c1stderr)
+	printf "${RED}$err${NC}\n"
+else 
+	printf "${GREEN}Start nginx.service${NC}\n"
+fi
